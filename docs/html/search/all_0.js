65eccff1c942bcc9e29ca50f5bd077f747fe5a59
pErfEcto2 <misha.yaro2004@gmail.com>
2025-03-27 16:43:39 +0300

    minor changes

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['queue_5fscheduler_0',['queue_scheduler',['../md_README.html',1,'']]]
+];
---
+++ docs/html/search/all_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['требования_3a_0',['Требования:',['../md_README.html#autotoc_md2',1,'']]]
+];
